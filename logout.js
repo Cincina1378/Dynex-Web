9cd3baf2acab5b773e9c8aedaf27157acaeb09de
Cincina1378 <tfyebirligiedittvshorts@gmail.com>
2026-08-22 13:32:33 +0300

    Update logout.js

--- logout.js
+++ logout.js
@@ -1,11 +1,10 @@
 exports.handler = async function () {
-
   return {
     statusCode: 302,
 
     multiValueHeaders: {
       "Set-Cookie": [
-        "discord_token=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0"
+        "dynex_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0"
       ]
     },
 
@@ -13,5 +12,4 @@ exports.handler = async function () {
       Location: "/"
     }
   };
-
 };
